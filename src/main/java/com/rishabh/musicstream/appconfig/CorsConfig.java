1ce8002d23c17ee4c2854fccd0e22fbf1adefd97
rishabh-147 <onlinestuff.rishabh@gmail.com>
2026-08-02 00:08:46 +0530

    [DEV] prod and cors enhancments

--- src/main/java/com/rishabh/musicstream/appconfig/CorsConfig.java
+++ src/main/java/com/rishabh/musicstream/appconfig/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.rishabh.musicstream.appconfig;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,21 +8,26 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Slf4j
 public class CorsConfig {
 
-    @Value("${app.frontend.url}")
-    private String frontendUrl;
+// Accept multiple frontend origins separated by commas.
+// Example:
+// FRONTEND_URLS=http://localhost:3000,https://rishabh-147.github.io
+    @Value("${app.frontend.urls}")
+    private String frontendUrls;
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
 
+        log.info("Cross Origin set to : {}", frontendUrls);
         return new WebMvcConfigurer() {
 
             @Override
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins(frontendUrl)
+                        .allowedOrigins(frontendUrls.split(","))
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
